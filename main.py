69c5dcdafae9b4416961e8d331a8dc1752736334
Lorenzo Felici <loryfelix42@gmail.com>
2026-03-19 11:37:47 +0100

    Created .gitignore file and python file now opens google

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import selenium 
+from selenium import webdriver 
+import time 
+service = "/home/lorenzo/Progetti/academic-portal-monitor/chromedriver-linux64/chromedriver"
+
+options = webdriver.ChromeOptions()
+options.binary_location = "/usr/bin/google-chrome-stable"
+
+driver = webdriver.Chrome(service=webdriver.ChromeService(executable_path=service), options=options)
+driver.get("https://www.google.com")
+time.sleep(5)
