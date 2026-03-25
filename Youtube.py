bddd992edc6ac046c73984870820d7f2c6d8d1e7
Lorenzo Felici <loryfelix42@gmail.com>
2026-03-25 22:43:50 +0100

    I updated the file Youtube.py and now it can accept the cookies, search a video, click on it and put it on full screen. I also created a cookies.json file where i will store my cookies sessions and added the file on .gitignore

--- Youtube.py
+++ Youtube.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time 
+
 service = "/home/lorenzo/Progetti/academic-portal-monitor/chromedriver-linux64/chromedriver"
 
 options = webdriver.ChromeOptions()
@@ -11,10 +12,23 @@ options.binary_location = "/usr/bin/google-chrome-stable"
 
 driver = webdriver.Chrome(service=webdriver.ChromeService(executable_path=service), options=options)
 driver.get("https://www.youtube.com/")
-time.sleep(15)    # wait for the user to log in
+#time.sleep(20)
 
-search = driver.find_element(By.NAME, "search_query")
-search.send_keys("lo-fi")
+try:
+    cookie_accept = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//*[@id='content']/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button/yt-touch-feedback-shape/div[2]"))
+    )
+    cookie_accept.click()
+except:
+    print("Cookie banner not found")
+
+time.sleep(5)
+try:
+    search = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "search_query"))
+    )
+finally:
+    search.send_keys("lo-fi")
 
 search.send_keys(Keys.RETURN)
 
@@ -28,5 +42,13 @@ finally:
     video.click()
     print("video cliccato")
 
-print(driver.title)
+try:
+    full_screen = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.XPATH, "//*[@id='movie_player']/div[24]/div[2]/div[2]/div[2]/button[3]"))
+    )
+finally:
+        full_screen.click()
+
+
+input("Press Enter to exit...")
 driver.quit()
